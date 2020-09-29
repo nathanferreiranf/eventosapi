daa00ed1eb2b099e12d5449e64350209b11a7b72
Nathan Ferreira <nathanferreiranf@gmail.com>
2020-09-29 14:31:36 -0300

    Ajustes

--- resources/views/emails/welcomeIBMAC.blade.php
+++ resources/views/emails/welcomeIBMAC.blade.php
@@ -2,22 +2,14 @@
 {{-- Header --}}
 @slot('header')
     @component('mail::header', ['url' => config('app.url')])
-        Header Title
+        
     @endcomponent
 @endslot
 
 {{-- Body --}}
-<table class="corpo-email-banner">
-    <tbody>
-        <tr>
-            <td>
-                <a href="{{ $data->redirect_url }}" style="width: 100%">
-                    <img src="https://ibmecsummit.com.br/evento-ibemac/public/images/pecas-email/banner.png" alt="Banner">
-                </a>
-            </td>
-        </tr>
-    </tbody>
-</table>
+<a href="{{ $data->redirect_url }}" style="width: 100%">
+    <img src="https://ibmecsummit.com.br/evento-ibemac/public/images/pecas-email/banner.png" alt="Banner">
+</a>
 
 {{-- Subcopy --}}
 @isset($subcopy)
@@ -31,7 +23,7 @@
 {{-- Footer --}}
 @slot('footer')
     @component('mail::footer')
-        © {{ date('Y') }} {{ config('app.name') }}. Super FOOTER!
+        © {{ date('Y') }} {{ $data->name_company }}.
     @endcomponent
 @endslot
 @endcomponent
