06eab646387cf65806b1aff0b70f4fc84ccf2905
Nathan Ferreira <nathanferreiranf@gmail.com>
2020-09-30 21:17:39 -0300

    Ajustes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:120,1');
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:rate_limit,1');
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::resource('/eventos', EventosController::class);
