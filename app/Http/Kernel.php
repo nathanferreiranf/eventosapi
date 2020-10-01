a7f2bfc6916bbc7a38ba1c73e26026c78e3a27b8
Nathan Ferreira <nathanferreiranf@gmail.com>
2020-10-01 08:45:15 -0300

    Ajustes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            'throttle:300,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
