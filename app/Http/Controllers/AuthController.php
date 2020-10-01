315eee5863a6ada6e3a7c7332e8b67ace7e469b6
Nathan Ferreira <nathanferreiranf@gmail.com>
2020-09-30 21:25:27 -0300

    Ajustes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,8 +34,7 @@ class AuthController extends Controller
             'user' => $user,
             'access_token' => $token->plainTextToken
         ]);*/
-
-        return response()->json($request->input);
+        return response()->json($request->input());
     }
     
     public function register(Request $request){
