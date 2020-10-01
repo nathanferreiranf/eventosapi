85022168d4b593ac0ebb54615a10f2ed42755e90
Nathan Ferreira <nathanferreiranf@gmail.com>
2020-09-30 23:07:08 -0300

    Ajustes

--- public/cliente.php
+++ public/cliente.php
@@ -1,4 +1,29 @@
-<?php
-    header('Content-Type: application/json');
-    echo json_encode($_POST);
+<?php 
+header('Content-Type: application/json');
+
+# Substitua abaixo os dados, de acordo com o banco criado
+$user = "root";
+$password = ""; 
+$database = "eventos"; 
+
+# O hostname deve ser sempre localhost 
+$hostname = "127.0.0.1"; 
+
+# Conecta com o servidor de banco de dados 
+$link = mysqli_connect($hostname, $user, $password, $database);
+
+//mysql_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' ); 
+//mysql_select_db( $database ) or die( 'Erro na seleção do banco' );
+
+$query = "SELECT * FROM users";
+$result_query = mysqli_query( $link, $query ) or die(' Erro na query:' . $query . ' ' . mysql_error() );
+
+$data = [];
+
+while ($row = mysqli_fetch_array( $result_query )) { 
+    array_push($data, $row);
+}
+
+
+echo json_encode($data);
 ?>
